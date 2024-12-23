9f980a52f6f7d406b49386436a93a9dd17f7c228
Fahad Hussain <mafh7212@gmail.com>
2024-12-23 10:20:21 +0500

    Add files via upload

---
+++ Polymorphism(Overriding)/Polymorphism(Overriding)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Overriding_
+{
+    internal class Program
+    {
+        class Animal
+        {
+            public virtual void Fly()
+            {
+                Console.WriteLine("Animal can fly");
+            }
+        }
+        class Bird : Animal
+        {
+            public override void Fly()
+            {
+                Console.WriteLine("Bird can fly");
+            }
+        }
+        class MyObj
+        {
+            static void Main(string[] args)
+            {
+                MyObj objPoly = new MyObj();
+                Animal obj1 = new Bird();
+                obj1.Fly();
+                Animal obj2 = new Animal();
+                obj2.Fly();
+                Console.ReadLine();
+            }
+        }
+    }
+}
